1e3c6d4e5f311a3125329689e31c4030009eaf1c
松岡諒 <[ryomatsu0120@gmail.com]>
2018-11-21 14:59:06 +0900

    update-view

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".nav-lists li").hover(
+    function(e) {
+      $(".nav-parent-lists__list",this).css('display', 'block');
+    },
+    // マウスポインターが画像から外れた時の動作
+    function(e) {
+    // thisの子要素取得
+      $(".nav-parent-lists__list",this).css('display', 'none');
+  });
+});
