82d48d5b8124aec2a060b9bc8ac2e0f4ce501981
松岡諒 <[ryomatsu0120@gmail.com]>
2018-12-22 15:54:04 +0900

    create_sign_up

---
+++ app/controllers/users/registration_controller.rb
@@ -0,0 +1,3 @@
+class Users::RegistrationController < Devise::RegistrationsController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/sign_up/registrations',
+  }
+
   root to: 'products#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:edit, :new]
+
+  namespace :users do
+    namespace :sign_up do
+      # get 'address', to: 'address#new'
+      # get 'payment_methods', to: 'payment_methods#new'
+      get 'registrations', to: 'registrations#new'
+
+      # resources :address, only: [:create]
+      # resources :payment_methods, only: [:create]
+      # resources :done, only: [:index]
+    end
+  end
+  # devise_scope :user do
+  #   get 'users', to: 'devise/sessions#new'
+  # end
 end
