fd51c9aad02676d26af557c5d9d9dc9d6b309567
松岡諒 <[ryomatsu0120@gmail.com]>
2019-01-06 19:56:56 +0900

    create-address-setting

---
+++ app/controllers/users/sign_up/address_controller.rb
@@ -0,0 +1,20 @@
+class Users::SignUp::AddressController < ApplicationController
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    binding.pry
+    if @address.save
+      redirect_to users_sign_up_payment_methods_path
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:family_name, :first_name, :family_name_kana,:first_name_kana, :zipcode, :prefecture, :city, :block_number, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
+
+end
