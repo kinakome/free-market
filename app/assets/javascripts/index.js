0fd3241f913e3cb5a2df2428527b09e421fa0a49
松岡諒 <[ryomatsu0120@gmail.com]>
2019-01-17 11:11:22 +0900

    create-view

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,12 +1,10 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
     $(".nav-lists li").hover(
-      function(e) {
+      function() {
         $(".nav-parent-lists__list",this).css('display', 'block');
       },
-      // マウスポインターが画像から外れた時の動作
-      function(e) {
-      // thisの子要素取得
+      function() {
         $(".nav-parent-lists__list",this).css('display', 'none');
     });
   });
